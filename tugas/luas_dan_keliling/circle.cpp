290b2ccf752a75fd8d91bef5ca61324c64b2dfc4
Ammar Faizi <ammarfaizi2@gmail.com>
2020-11-28 17:09:50 +0700

    Update circle.cpp

--- tugas/luas_dan_keliling/circle.cpp
+++ tugas/luas_dan_keliling/circle.cpp
@@ -10,14 +10,14 @@ int main(int argc, char const *argv[])
 {
 
 	float r;
-	const float phi =  3.14159265359;
+	const float pi =  3.14159265359;
 
 	std::cout << "[Program Perhitungan Luas dan Keliling Lingkaran]\n";
 	std::cout << "Masukkan jari-jari lingkaran: ";
 	std::cin >> r;
 
-	std::cout << "\nKeliling lingkaran\t = " << (2 * phi * r);
-	std::cout << "\nLuas lingkaran\t\t = " << (phi * r * r);
+	std::cout << "\nKeliling lingkaran\t = " << (2 * pi * r);
+	std::cout << "\nLuas lingkaran\t\t = " << (pi * r * r);
 	std::cout << "\n";
 
 	return 0;
